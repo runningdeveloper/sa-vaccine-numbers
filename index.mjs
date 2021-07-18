44cb17064069db8ecf8ce5e8d961aafc69d30350
Geoffrey <info@runningdeveloper.com>
2021-07-18 17:16:57 +0200

    worked?

--- index.mjs
+++ index.mjs
@@ -4,8 +4,7 @@ import { parseNumbers } from './utils.mjs';
 import JSONdb from 'simple-json-db'
 const db = new JSONdb('./public/db.json');
 
-const url = `https://app.powerbi.com`
-// `https://app.powerbi.com/view?r=eyJrIjoiMDNlNTMyZWUtYjkyYS00NGE1LTliZTktZDI4MDU0ZTU0OTk1IiwidCI6ImE1MTczNzFjLWYzMTYtNDg0Yy1hYzVjLTk4Yjc2MTI3NzkwYSIsImMiOjl9`
+const url = `https://app.powerbi.com/view?r=eyJrIjoiMDNlNTMyZWUtYjkyYS00NGE1LTliZTktZDI4MDU0ZTU0OTk1IiwidCI6ImE1MTczNzFjLWYzMTYtNDg0Yy1hYzVjLTk4Yjc2MTI3NzkwYSIsImMiOjl9`
 
 const browser = await puppeteer.launch({
     headless: true,
@@ -14,15 +13,6 @@ const browser = await puppeteer.launch({
 const page = await browser.newPage();
 
 await page.goto(url, { waitUntil: 'networkidle2' }).catch(e => {throw new Error(e)});
-
-// wait for the nav icons
-await page.waitForSelector(`img`, {
-    visible: true,
-})
-
-console.log('made it')
-
-throw new Error('test done crash now')
 
 // wait for the nav icons
 await page.waitForSelector(`.navigation-wrapper`, {
