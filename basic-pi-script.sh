a0b9ccafbec575febf6bf363f14146ae7adfdae8
Pi <info@runningdeveloper.com>
2021-07-20 20:22:31 +0200

    pi script

---
+++ basic-pi-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# super simple script to run on the raspberry pi and commit the update
+
+git pull
+ISPI=true node index.mjs
+git commit -m "scrape update" -- ./public/db.json
+git push
+echo "Done!"
